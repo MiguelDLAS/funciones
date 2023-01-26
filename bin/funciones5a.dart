830e5650900b7c05212810c57052bdd9830aac0c
MiguelDLAS <mikiserrano2018@gmail.com>
2023-01-25 19:18:25 -0600

    dart

---
+++ bin/funciones5a.dart
@@ -0,0 +1,5 @@
+import 'package:funciones5a/funciones5a.dart' as funciones5a;
+
+void main(List<String> arguments) {
+  print('Hello world: ${funciones5a.calculate()}!');
+}
---
+++ test/funciones5a_test.dart
@@ -0,0 +1,8 @@
+import 'package:funciones5a/funciones5a.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
